3d070e1604d91b0b7804190bf5ebed0c1882df64
Ildar Mulyukov <ildar@altlinux.ru>
2018-04-28 02:33:22 +0600

    fix building with LIBNFC_DRIVER_RC522_UART=ON

--- libnfc/timing.c
+++ libnfc/timing.c
@@ -14,7 +14,8 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
- 
+#define _POSIX_C_SOURCE 199309L
+
 #include "timing.h"
 #include <assert.h>
 
